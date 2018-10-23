e319618bacbf3d14902f04268170b91e2749b018
kjakx <43692824+kjakx@users.noreply.github.com>
2018-10-23 22:03:19 +0900

    クラス化したいのでまずはインターフェースを書いた

---
+++ bitFlyerAPI.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+class BF_API():
+
+    def __init__(self, endpoint_url):
+
+        self.URL = endpoint_url
+
+    def _req2dic(self, url):
+
+        resp = requests.get(url)
+        dic = json.loads(resp.content)
+        return dic
+
+    def _urlBuilder(self, path, **query):
+
+        url = self.URL + path + '/'
+        if len(query) != 0:
+            url += '?'
+            for i, (key, value) in enumerate(query.items(), 1):
+                url += (key + '=' + str(value))
+                if i != len(query):
+                    url += '&'
+        return url
+
+    def _call(self, request, **query):
+
+        req = request.split(' ')
+        method = req[0]
+        path = req[1]
+        url = self._urlBuilder(path, **query)
+        dic = self._req2dic(url)
+        return dic
+
+# test
+if __name__ == "__main__":
+    api = BF_API("https://api.bitflyer.com")
+    print(api._call("GET /v1/getexecutions", product_code="BTC_JPY", count=1, before=0, after=0))
